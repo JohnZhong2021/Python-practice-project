3524666969dde1d1bcbfa7a992cd273228e665b0
JohnZhong <wpzhongqiang@hotmail.com>
2021-11-04 19:41:48 +0800

    fix some output prompts

--- code/Email_slicer.py
+++ code/Email_slicer.py
@@ -5,24 +5,24 @@ Created on Sat Oct 30 16:17:39 2021
 @author: JohnZhong
 """
 
-def email_user(mail):
-    i = 0
-    for char in mail:
-        if char == "@":
-           break
-        else:
-           i += 1
-    user_name = mail[0: i]
-    return user_name
-def email_domain(mail):
-    i = 0
-    for char in mail:
-        if char == "@":
-           break
-        else:
-           i += 1
-    domain_name = mail[i+1: ]
-    return domain_name
-x = input("Please input an email address:\n")
-print("The user name is:", email_user(x))
-print("The domain name is:", email_domain(x))
+class slicer(object):
+    def __init__(self, mail):
+        self.mail = mail
+    def get_slice_num(self):
+        i = 0
+        for char in self.mail:
+            if char == "@":
+                break
+            else:
+                i += 1
+        return i
+    def get_user_name(self):
+        user_name = self.mail[0:self.get_slice_num()]
+        return user_name
+    def get_domain_name(self):
+        domain_name = self.mail[self.get_slice_num()+1:]
+        return domain_name
+x = slicer(input("Please input an email address:\n"))
+print("The user name is:",x.get_user_name())
+print("The domain name is:",x.get_domain_name())
+
