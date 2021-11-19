50dd567a99e963668f9dae6d5308aae17ce28e07
JohnZhong <wpzhongqiang@hotmail.com>
2021-11-19 20:48:52 +0800

    Add fibonacci numbers in recursive version

---
+++ code/09Fibonacci_number-recursive_version.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 19:51:59 2021
+
+@author: JohnZhong
+"""
+
+
+def fibonacci_num(i):
+    if i == 1 or i == 2:
+        return 1
+    else:
+        return fibonacci_num(i - 1) + fibonacci_num(i - 2)
+
+
+for i in range(1, 25):
+    print(fibonacci_num(i), end=" ")
