6c6df81d6c0818c992d584d07cbf8019d949bb03
JohnZhong <wpzhongqiang@hotmail.com>
2021-11-21 20:51:24 +0800

    add fibonacci number in iterative version, in this version, the time complexity is lower than recursive version

---
+++ code/11Fibonacci_num-iterative_version.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 19 18:51:59 2021
+
+@author: zhongqiang
+"""
+
+def fibonacci_num(i):
+    j = 3
+    f1 = 1
+    f2 = 1
+    if i == 1 or i == 2:
+        return 1
+    else:
+        while j <= i:
+            f = f1 + f2
+            f2 = f1
+            f1 = f
+            j += 1
+        return f
+for i in range(1, 80):
+    print(fibonacci_num(i), end=" ")
+
