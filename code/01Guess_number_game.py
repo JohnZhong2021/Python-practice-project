737a680c0c3ef004c5bc429f599fd99e176fca18
JohnZhong <wpzhongqiang@hotmail.com>
2021-11-28 10:24:51 +0800

    fix some bugs

--- code/01Guess_number_game.py
+++ code/01Guess_number_game.py
@@ -1,39 +1,41 @@
-# -*- coding: utf-8 -*-
-
 import random
 
-def get_num_guess(n1, n2):
-    number_guess = random.randint(n1, n2)
-    return number_guess
 
-def get_user_guess(n1, n2):
-    print("The number is between: ", n1, "-", n2)
+def get_num_guess(int1, int2):
+    number_generate = random.randint(int1, int2)
+    return number_generate
+
+
+def get_user_guess(int1, int2):
+    print("------------\nThe number is between: ", int1, "-", int2, "\n-------------")
     while True:
         try:
-            user_guess = int(input("Please guess a number: "))
-            if user_guess in range(n1, n2):
-                return user_guess
-                break
+            user_input = int(input("Please try a number: "))
+            if user_input in range(int1, int2 + 1):
+                return user_input
             else:
                 print("The number is out of range, please give another try!")
         except(ValueError, SyntaxError):
-            print("Invalid data, plese enter a number!")
+            print("Invalid data, please enter a number!")
 
-def get_guess_count(number_guess, user_guess):
+
+def get_guess_count(number_generate, user_input, int1, int2):
     j = 1
-    while user_guess != number_guess:
-        if user_guess > number_guess:
-            print("Wrong guess! The number is too large!")
-            user_guess = int(input("Please try another number: "))
-        elif user_guess < number_guess:
-            print("Wrong guess! The number is too small!")
-            user_guess = int(input("Please try another number: "))
+    while number_generate != user_input:
+        if user_input > number_generate:
+            print("\nWrong guess! The number is too large!")
+            user_input = get_user_guess(int1, int2)
+        elif user_input < number_generate:
+            print("\nWrong guess! The number is too small!")
+            user_input = get_user_guess(int1, int2)
         j += 1
-    print("\n*****\nBingo! Congretulations! You are right!!")
+    print("\n*****\nBingo! Congratulations! You are right!!")
     return j
+
+
 n1 = 1
-n2 =100
+n2 = 100
 number_guess = get_num_guess(n1, n2)
 user_guess = get_user_guess(n1, n2)
-guess_count = get_guess_count(number_guess, user_guess)
-print("Your guess count is", guess_count, "\n*****")
+guess_count = get_guess_count(number_guess, user_guess, n1, n2)
+print("Your guess count is:", guess_count, "\n*****")
